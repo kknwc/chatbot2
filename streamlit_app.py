3c162d66802b378ac9ff75ba40061620326e0f3b
kknwc <tiffloke@gmail.com>
2024-11-14 21:18:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,12 +79,18 @@ with st.sidebar:
             saved_conversations.insert(0, list(st.session_state.messages)) # Insert at beginning to maintain order
             st.sidebar.success("Previous conversation has been saved.")
             st.session_state.messages = [initial_message] # Resets chat
+            st.sidebar.success("work 1")
             save_chat_history(st.session_state.messages) # Save empty conversation (or initial state) to shelve
+            st.sidebar.success("work 2")
         else:
             st.session_state.saved_conversations = update_saved_conversation(current_conversation, saved_conversations)
+            st.sidebar.success("work 3")
             st.sidebar.info("No new updates; conversation saved and remains unchanged.")
+            st.sidebar.success("work 4")
             st.session_state.messages = [initial_message] # Resets chat
+            st.sidebar.success("work 5")
             save_chat_history(st.session_state.messages) # Save empty conversation (or initial state) to shelve
+            st.sidebar.success("work 6")
 
         # Update saved conversations list without duplicating identical conversations
         # st.session_state.saved_conversations = update_saved_conversation(current_conversation, saved_conversations)
