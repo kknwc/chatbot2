486e3f2c67434ced409e39796071f678e3c74c74
kknwc <tiffloke@gmail.com>
2024-11-15 23:44:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ authenticator = stauth.Authenticate(
         cookie_expiry_days=30
 )
         
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login("Login", location="main")
 
 if authentication_status == False:
         st.error("Incorrect Username or Password.")
